c4cd8d0bb2670533671192f0ee89e8e7e880908b
Michele Cecchi <130759213+Rochettoro@users.noreply.github.com>
2023-05-06 13:48:18 +0200

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,27 @@
+/**
+ * @autor Michele Cecchi
+ */
+
+import java.time.LocalDate;
+
+public class Main {
+
+        public static void main(String[] args) {
+
+            // Vari metodi per ottenere il codice fiscale
+
+            CalcoloCodiceFiscale codiceFiscale = new CalcoloCodiceFiscale("Mario", "Rossi", "M", LocalDate.of(1990,1,1), "Roma");
+            //CalcoloCodiceFiscale codiceFiscale = new CalcoloCodiceFiscale("Mario", "Rossi", "M", LocalDate.of(1990,1,01), "Roma");
+            //CalcoloCodiceFiscale codiceFiscale = new CalcoloCodiceFiscale("Mario", "Rossi", "M", LocalDate.of(1990,01,1), "Roma");
+            //CalcoloCodiceFiscale codiceFiscale = new CalcoloCodiceFiscale("Mario", "Rossi", "M", LocalDate.of(1990,01,01), "Roma");
+            //CalcoloCodiceFiscale codiceFiscale = new CalcoloCodiceFiscale("Mario", "Rossi", "M", "1990", "1", "1", "Roma");
+            //CalcoloCodiceFiscale codiceFiscale = new CalcoloCodiceFiscale("Mario", "Rossi", "M", "1990", "1", "01", "Roma");
+            //CalcoloCodiceFiscale codiceFiscale = new CalcoloCodiceFiscale("Mario", "Rossi", "M", "1990", "01", "1", "Roma");
+            //CalcoloCodiceFiscale codiceFiscale = new CalcoloCodiceFiscale("Mario", "Rossi", "M", "1990", "01", "01", "Roma");
+
+
+            System.out.println(codiceFiscale);                              // Codice fiscale ottenuto con il programma
+            System.out.println("Codice fiscale: RSSMRA90A01H501W");         // Codice fiscale ottenuto con https://codicefiscale.it/ e con http://www.codicefiscaleonline.com/
+
+        }
+}
